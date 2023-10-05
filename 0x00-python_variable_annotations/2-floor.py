b7a6873d3dc7eca6134c6532ebd4e1aa3f18b8a8
Dark Mind <dune934@gmail.com>
2023-10-05 16:38:46 +0300

    This is for task 4

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,7 +4,7 @@ a type-annotated file
 """
 
 
-def floor (n: float) -> int:
+def floor(n: float) -> int:
     """type-annotated function floor which takes a float n
     as argument and returns the floor of the float"""
     return int(n)
