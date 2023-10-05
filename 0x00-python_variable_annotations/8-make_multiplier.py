31682227664e5b6e1015b591d788bf6ecfb3b9bf
Dark Mind <dune934@gmail.com>
2023-10-05 17:38:10 +0300

    for task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+a type-annotated file
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """type-annotated function make_multiplier that takes a float multiplier
+    as argument and returns a function that multiplies a float by
+    multiplier"""
+
+    def multi_float(multi: float) -> float:
+        """Multiples the parameter with the parent function parameter"""
+        return multi * multiplier
+    
+    return multi_float
