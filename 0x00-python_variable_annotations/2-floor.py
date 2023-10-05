160b67c88de2600e52a4e564c539501d607661cb
Dark Mind <dune934@gmail.com>
2023-10-05 16:24:37 +0300

    This is for task 3

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """
-#!/usr/bin/env python3
+a type-annotated file
 """
 
 
 def floor (n: float) -> int:
+    """type-annotated function floor which takes a float n
+    as argument and returns the floor of the float"""
     return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a type-annotated file
+"""
+
+
+def to_str(n: float) -> str:
+    """type-annotated function to_str that takes a float n as argument
+    and returns the string representation of the float"""
+    return str(n)
