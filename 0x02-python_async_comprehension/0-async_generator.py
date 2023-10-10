cd878175932f716b33cd29acba5165c41e5bb6c6
Samuel <a2jsamuel@gmail.com>
2023-10-10 22:23:04 +0300

    fix and add

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ a coroutine called async_generator that takes no arguments.
 """
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """The coroutine will loop 10 times, each time asynchronously
     wait 1 second, then yield a random number between 0 and 10"""
     for x in range(10):
