9e818a5a59fff0ea66679e9b7fe4f549b2ef5546
Samuel <a2jsamuel@gmail.com>
2023-10-10 16:40:54 +0300

    this is for task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+an asynchronous coroutine file
+"""
+import asyncio
+from typing import List
+waiting_3 = __import__('3-tasks').task_wait_random
+# waiting = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays (float values). The list of the
+    delays should be in ascending order without using sort() because
+    of concurrency."""
+    wait_x: List[float] = []
+    total_wait: List[float] = []
+
+    for x in range(n):
+        wait_x.append(waiting_3(max_delay))
+
+    """ it is bc of listing the results in
+    ascending order without using sort method we use as_completed!!!"""
+    for y in asyncio.as_completed(wait_x): 
+        z = await y
+        total_wait.append(z)
+
+    return total_wait
