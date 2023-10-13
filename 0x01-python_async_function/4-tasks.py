8d45a4b244a420b40c88472484ff0d02e10f292a
Samuel <a2jsamuel@gmail.com>
2023-10-13 22:41:10 +0300

    Fix and see for 4-file!

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
-""" Take the code from wait_n and alter it into a new function task_wait_n.
-    The code is nearly identical to wait_n except task_wait_random is being
-    called. """
+"""
+an asynchronous coroutine file
+"""
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+waiting = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-        """Tasks"""
-        delays: List[float] = []
-        all_delays: List[float] = []
-        for i in range(n):
-            delays.append(task_wait_random(max_delay))
-        for delay in asyncio.as_completed(delays):
-            earliest_result = await delay
-            all_delays.append(earliest_result)
-        return all_delays
+    """return the list of all the delays (float values). The list of the
+    delays should be in ascending order without using sort() because
+    of concurrency."""
+    wait_x: List[float] = []
+    total_wait: List[float] = []
+
+    for x in range(n):
+        wait_x.append(waiting(max_delay))
+
+    for y in asyncio.as_completed(wait_x):
+        zee = await y
+        total_wait.append(zee)
+
+    return total_wait
