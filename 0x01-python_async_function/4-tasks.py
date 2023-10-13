4fd64f581d321d4ddb1733ad7fab678653a5645e
Samuel <a2jsamuel@gmail.com>
2023-10-13 22:37:02 +0300

    Test and see

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,24 +1,18 @@
-#!/usr/bin/env python3
-"""
-an asynchronous coroutine file
-"""
+""" Take the code from wait_n and alter it into a new function task_wait_n.
+    The code is nearly identical to wait_n except task_wait_random is being
+    called. """
 import asyncio
 from typing import List
-waiting = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all the delays (float values). The list of the
-    delays should be in ascending order without using sort() because
-    of concurrency."""
-    wait_x: List[float] = []
-    total_wait: List[float] = []
-
-    for x in range(n):
-        wait_x.append(waiting(max_delay))
-
-    for y in asyncio.as_completed(wait_x):
-        z = await y
-        total_wait.append(z)
-
-    return total_wait
+        """Tasks"""
+        delays: List[float] = []
+        all_delays: List[float] = []
+        for i in range(n):
+            delays.append(task_wait_random(max_delay))
+        for delay in asyncio.as_completed(delays):
+            earliest_result = await delay
+            all_delays.append(earliest_result)
+        return all_delays
