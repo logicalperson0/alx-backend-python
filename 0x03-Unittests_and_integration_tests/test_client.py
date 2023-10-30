df2d72809794019f7a16c90647bf584fb14d28b1
Samuel <a2jsamuel@gmail.com>
2023-10-30 18:45:06 +0300

    this is for task 4: test_org

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+a module with class: TestGithubOrgClient to test
+client.GithubOrgClient func
+"""
+import unittest
+from client import GithubOrgClient
+from parameterized import parameterized
+import requests
+from typing import Dict
+from unittest.mock import patch, Mock, MagicMock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """class that inherits from unittest.TestCase"""
+    @parameterized.expand([
+        ("google", {'login': 'google'}),
+        ("abc", {'login': 'abc'}),
+    ])
+    @patch('client.get_json')
+    def test_org(self, orgs, supposed, mock_fn):
+        """ method should test that GithubOrgClient.org returns
+        the correct value"""
+        mock_fn.return_value = Mock(return_value=supposed)
+        # mock_fn.return_value = supposed
+        # print(mock_fn)
+        actual = GithubOrgClient(orgs).org()
+        # print(actual)
+
+        self.assertEqual(actual, supposed)
